5881a70c2cec388d14080e6c5a53847d7f2a1bdc
Stalin Carrión <sfcarrion@utpl.edu.ec>
2018-10-01 16:14:59 -0500

    Mostrar imagenes swg

---
+++ ImagenSWG/Assets/ChangeImg.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+//using Unity.VectorGraphics;
+
+
+
+public class ChangeImg : MonoBehaviour {
+
+    public RawImage img;
+   
+    private void Awake()
+    {
+        img = this.gameObject.GetComponent<RawImage>();
+    }
+    IEnumerator Start()
+    {
+        WWW www = new WWW("https://commons.wikimedia.org/wiki/File:Ghostscript_Tiger.svg");
+        yield return www ;
+        
+        img.texture = www.texture;
+        
+        
+    }
+
+
+}
